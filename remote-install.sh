38c5d41db55bd35b35e58a53f4b3f4ee3d40b220
Atzen <attila@some.one>
2019-05-30 16:54:01 +0200

    added options to disable root ssh access

--- remote-install.sh
+++ remote-install.sh
@@ -12,6 +12,10 @@ SUDO_USER_PWD="<password of to be created sudo user>"
 KNOWN_HOSTS_FILE_PATH="/home/<your user name>/.ssh/known_hosts"
 IS_MAC=true
 
+# use with caution. If you want to revoke your decission later, you manually need to edit /etc/ssh/sshd_config
+DISABLE_ROOT_LOGIN=false 
+DISABLE_ROOT_PASSWORD_LOGIN=false
+
 
 ###################################################################################################
 # DEFINES
@@ -21,6 +25,10 @@ CREATE_SUDO_USER_SCRIPT_NAME="create-sudo-user.sh"
 
 INSTALL_ARDOR_SCRIPT_NAME="install-ardor.sh"
 INSTALL_ARDOR_SCRIPT_PATH=$(dirname `which $0`)
+
+CREATE_SUDO_USER_CMD="chmod 700 ${CREATE_SUDO_USER_SCRIPT_NAME} && ./${CREATE_SUDO_USER_SCRIPT_NAME} -u ${SUDO_USER_NAME} -p ${SUDO_USER_PWD}"
+DISABLE_ROOT_LOGIN_CMD='sed -i -e "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config'
+DISABLE_ROOT_PASSWORD_LOGIN_CMD='sed -i -e "s/PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config'
 
 
 ###################################################################################################
@@ -33,8 +41,13 @@ else
     ssh-keygen -f ${KNOWN_HOSTS_FILE_PATH} -R ${SERVER_DOMAIN}
 fi
 
+SERVER_ACCESS_CMD=""
+if [ ${DISABLE_ROOT_LOGIN} == true ]; then SERVER_ACCESS_CMD="${DISABLE_ROOT_LOGIN_CMD} && "; fi
+if [ ${DISABLE_ROOT_PASSWORD_LOGIN} == true ]; then SERVER_ACCESS_CMD="${SERVER_ACCESS_CMD}${DISABLE_ROOT_PASSWORD_LOGIN_CMD} && "; fi
+SERVER_ACCESS_CMD="${SERVER_ACCESS_CMD}${CREATE_SUDO_USER_CMD}"
+
 scp ./${CREATE_SUDO_USER_SCRIPT_NAME} root@${SERVER_DOMAIN}: | tee log.txt
-ssh -t root@${SERVER_DOMAIN} "chmod 700 ${CREATE_SUDO_USER_SCRIPT_NAME} && ./${CREATE_SUDO_USER_SCRIPT_NAME} -u ${SUDO_USER_NAME} -p ${SUDO_USER_PWD}" | tee -a log.txt
+ssh -t root@${SERVER_DOMAIN} "${SERVER_ACCESS_CMD}" | tee -a log.txt
 
 scp ${INSTALL_ARDOR_SCRIPT_PATH}/${INSTALL_ARDOR_SCRIPT_NAME} ${SUDO_USER_NAME}@${SERVER_DOMAIN}: | tee -a log.txt
 ssh -t ${SUDO_USER_NAME}@${SERVER_DOMAIN} "chmod 700 ${INSTALL_ARDOR_SCRIPT_NAME} && ./${INSTALL_ARDOR_SCRIPT_NAME}" | tee -a log.txt
